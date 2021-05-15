604e5e78b25bc881d70fa18eb6c5c19093feda5d
ryushin0316 <the.animal.kingdom18@gmail.com>
2021-05-15 16:23:33 +0900

    商品詳細１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,30 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.order("created_at DESC")
+    @items = Item.all
   end
 
   def new
-    @item = Item.new
+    if user_signed_in?
+     @item = Item.new      
+    else
+      redirect_to user_session_path(@item.id)
+    end
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to items_path
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
