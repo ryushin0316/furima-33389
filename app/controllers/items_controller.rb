2e1b4747f87477165b8f766e08806699829018a8
ryushin0316 <the.animal.kingdom18@gmail.com>
2021-05-19 10:20:13 +0900

    商品削除機能３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,7 @@ class ItemsController < ApplicationController
       if @item.destroy
         redirect_to root_path
       else
-        redirect_to edit_item_path
+        redirect_to item_path(item.id)
       end
     end
   end
